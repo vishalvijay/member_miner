1df1984be277f61a205de170c581c0f71d5917d1
Vishal Vijay <vishalvijay@jiffstore.com>
2015-10-10 17:49:17 +0530

    Add commects to code

--- app/controllers/api/v1/main_controller.rb
+++ app/controllers/api/v1/main_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::MainController < AppController
   def search_caption
     q = params[:query]
     if q.present?
-      s = Sunspot.search(Member) do
+      s = Sunspot.search(Member) do #make scalable search with solr
         fulltext("\"#{q}\"^5 OR #{q}~1") do
           fields(caption: 5)
           phrase_fields(caption: 5)
         end
-        paginate page: (params[:page]||1), per_page: (params[:limit]||25)
+        paginate page: (params[:page]||1), per_page: (params[:limit]||25) #pagination
       end
       respond_with s.results
     else
@@ -23,10 +23,10 @@ class Api::V1::MainController < AppController
   def ethnicity_averages
     ethnicity = params[:ethnicity]
     if ethnicity.present?
-      if Member::ETHNIC_GROUP.values.include?(ethnicity)
+      if Member::ETHNIC_GROUP.values.include?(ethnicity) #validate ethnicity
         members = Member.where ethnicity: ethnicity
-        mean_height = members.sum(:height)/members.count
-        mean_weight = members.sum(:weight)/members.count
+        mean_height = members.sum(:height)/members.count #calculating mean
+        mean_weight = members.sum(:weight)/members.count #calculating mean
         respond_with({mean_height: mean_height, mean_weight: mean_weight})
       else
         respond_errors "Unkown ethnicity", :not_found
@@ -39,6 +39,7 @@ class Api::V1::MainController < AppController
   def social_habits
     vegetarian = params[:vegetarian]
     drink = params[:drink]
+    #convert yes or no to boolean,
     if vegetarian.present?
       vegetarian = if vegetarian.downcase == "yes"
         true
@@ -58,7 +59,7 @@ class Api::V1::MainController < AppController
       end
     end
     if !vegetarian.nil? || !drink.nil?
-      s = Sunspot.search(Member) do
+      s = Sunspot.search(Member) do #make scalable search with solr
         with(:is_veg, vegetarian) unless vegetarian.nil?
         with(:drink, drink) unless drink.nil?
         paginate page: (params[:page]||1), per_page: (params[:limit]||25)
