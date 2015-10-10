8fddbfc741f3276307cdc038049109458b74c7fa
Vishal Vijay <vishalvijay@jiffstore.com>
2015-10-10 23:29:03 +0530

    Fix active admin form

--- app/admin/member.rb
+++ app/admin/member.rb
@@ -1,17 +1,3 @@
 ActiveAdmin.register Member do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+  permit_params :id, :caption, :ethnicity, :weight, :height, :is_veg, :drink, :dob
 end
